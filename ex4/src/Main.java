4023ed3628674a6f7e3babb42fca04c9cd61ae0b
André Hopf <andre.hopf@gmail.com>
2023-02-13 09:56:59 -0300

    Exercício 4

---
+++ ex4/src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    //Mike tem um nro da sorte: 25.
+    //Ele conseguiu uma listagem do histórico dos resultados da loteria conforme arrays abaixo.
+    //Escreva um código para percorrer os elementos de nros sorteados e
+    //contabilizar quantas vezes o nro da sorte dele foi sorteado.
+    //
+    //Dica: Usar um for dentro do outro para percorrer os arrays internos.
+    //int sorteados[][] = {
+    //    { 1, 44,  6,  2, 45, 60},
+    //    {10, 21, 55, 25, 34, 44},
+    //    { 8, 18, 28, 29, 55, 59},
+    //    {60, 25, 11, 34,  6,  9},
+    //    {55, 43, 25, 12,  7, 11}
+    //};
+
+    public static void main(String[] args) {
+        int sorteados[][] = {
+                { 1, 44,  6,  2, 45, 60},
+                {10, 21, 55, 25, 34, 44},
+                { 8, 18, 28, 29, 55, 59},
+                {60, 25, 11, 34,  6,  9},
+                {55, 43, 25, 12,  7, 11}
+        };
+
+        int numeroDaSorteSorteado = 0;
+
+        for (int[] array : sorteados) {
+            for (int numero : array) {
+                if (numero == 25) {
+                    numeroDaSorteSorteado++;
+                }
+            }
+        }
+
+        System.out.printf("O número da sorte de Mike (25) foi sorteado %d vezes.", numeroDaSorteSorteado );
+
+    }
+}
